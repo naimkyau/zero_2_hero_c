1ace28faf4b9bfaa759828304ca45bd8ba77f0b9
Muhammad Naim <naimahmed.srj@gmail.com>
2024-11-17 12:47:44 +0600

    sizeof

---
+++ 03_size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("char size \t= %d\n", sizeof(char));
+    printf("short size \t= %d\n", sizeof(short));
+    printf("short int size \t= %d\n", sizeof(short int));
+    printf("int size \t= %d\n", sizeof(int));
+    printf("long int size \t= %d\n", sizeof(long int));
+    printf("float size \t= %d\n", sizeof(float));
+    printf("double size \t= %d\n", sizeof(double));
+    printf("long double size =%d\n", sizeof(long double));
+    return 0;
+}
