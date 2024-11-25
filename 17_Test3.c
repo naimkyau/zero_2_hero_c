8157d03ad7c635e82b8fbbdce07fc90babc0d5c4
Muhammad Naim <naimahmed.srj@gmail.com>
2024-11-25 07:41:51 +0600

    Test 4

---
+++ 17_Test3.c
@@ -0,0 +1,15 @@
+
+// write a function { without argument & without return value }
+
+#include <stdio.h>
+
+void printsms(){
+    printf("\nHello! This is a function without arguments and return value.\n\n");
+}
+
+int main() {
+    
+    printsms();
+
+    return 0;
+}
