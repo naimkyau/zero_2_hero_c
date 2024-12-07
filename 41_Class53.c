a3ec7bdc2a397dcbd01c3036eda1229509f163b9
Muhammad Naim <naimahmed.srj@gmail.com>
2024-12-07 16:11:03 +0600

    Class 53

---
+++ 41_Class53.c
@@ -0,0 +1,17 @@
+
+// NULL pointer
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 34;
+    int * ptr = NULL;
+    if (ptr != NULL){
+    printf("The address of a is %d\n", ptr);
+    }
+    else{
+        printf("The pointer is a null pointer and cannot be dereferenced");
+    }
+    return 0;
+}
