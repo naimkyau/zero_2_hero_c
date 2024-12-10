11a2f53c583f3d1218f6f0c1902cdaf7aef236b9
Muhammad Naim <naimahmed.srj@gmail.com>
2024-12-10 07:58:30 +0600

    Class 67

---
+++ 52_Class67.c
@@ -0,0 +1,52 @@
+
+//      More about file I/O
+
+#include <stdio.h>
+
+int main() {
+    FILE *ptr;
+
+    // Mode: r (read)
+    ptr = fopen("my3.txt", "r");
+    char buffer[50];
+    fgets(buffer, sizeof(buffer), ptr);
+    printf("Read using 'r': %s\n", buffer);
+    fclose(ptr);
+
+    // Mode: w (write, overwrite)
+    ptr = fopen("my3.txt", "w");
+    fputs("This is written in 'w' mode.\n", ptr);
+    fclose(ptr);
+
+    // Mode: a (append)
+    ptr = fopen("my3.txt", "a");
+    fputs("This line is appended using 'a' mode.\n", ptr);
+    fclose(ptr);
+
+    // Mode: r+ (read/write)
+    ptr = fopen("my3.txt", "r+");
+    fgets(buffer, sizeof(buffer), ptr);
+    printf("Read using 'r+': %s", buffer);
+    fputs("Added this in 'r+' mode.\n", ptr);
+    fclose(ptr);
+
+    // Mode: w+ (write/read, overwrite)
+    ptr = fopen("my3.txt", "w+");
+    fputs("This is written in 'w+' mode.\n", ptr);
+    rewind(ptr);
+    fgets(buffer, sizeof(buffer), ptr);
+    printf("Read using 'w+': %s", buffer);
+    fclose(ptr);
+
+    // Mode: a+ (append/read)
+    ptr = fopen("my3.txt", "a+");
+    fputs("Appended using 'a+' mode.\n", ptr);
+    rewind(ptr);
+    printf("Content after 'a+':\n");
+    while (fgets(buffer, sizeof(buffer), ptr)) {
+        printf("%s", buffer);
+    }
+    fclose(ptr);
+
+    return 0;
+}
